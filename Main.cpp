a1aac75a6ef8e5eca5dd033bd3b5e6e2e3d2196a
sunnysteinway <76795691+sunnysteinway@users.noreply.github.com>
2021-08-10 09:21:34 +0800

    Create Main.cpp

---
+++ Main.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <string>
+#include <vector>
+
+#include "Screen.h"
+#include "WindowManager.h"
+
+using namespace std;
+
+int main()
+{
+	//WindowManager mgr;
+	Screen myscreen(24, 80, 'a');
+	//Screen myscreen2(1080, 1980, 'a', 0);
+
+	myscreen.Move(2, 3).Set('#');
+	Screen temp = myscreen.move(9, 9);
+
+	myscreen.Setter(0, 0) = '@';
+
+	myscreen.Display(std::cout);
+
+	const Screen blank(5, 3, ' ');
+	//blank.Display(std::cout);
+
+	Screen::pos ht = 24, wd = 80;	// use the pos type defined by Screen
+	Screen scr(ht, wd, ' ');
+	Screen* p = &scr;
+	char c = p->Get();	// fetches the get member function from the object to which p points
+  
+	system("pause");
+}
